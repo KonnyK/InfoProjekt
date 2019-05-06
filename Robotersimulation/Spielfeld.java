39128cf13f706cbbfb9d2a788cf2db9e93da8a6b
Konny <konrad.kroeber@freenet.de>
2019-05-06 14:41:24 +0200

    Spielfeld für Aufgabe3 bearbeitet

--- Robotersimulation/Spielfeld.java
+++ Robotersimulation/Spielfeld.java
@@ -19,13 +19,14 @@ public class Spielfeld
         System.out.println("-----------------------------------");
         System.out.println("Wieviele Punkte sollen abgefahren werden?");
         int Anzahl = 0;
-        boolean x = false;
+        boolean EingabeKorrekt = false;
         while (true)
         {
             try
             {
                 Anzahl = Scan.nextInt();
-                x = (Anzahl > 0);
+                if (Anzahl < 0) System.out.println("Bitte geben Sie eine Zahl größer null ein.");
+                EingabeKorrekt = (Anzahl > 0);
                 break;
             }
             catch (Exception E)
@@ -34,34 +35,57 @@ public class Spielfeld
                 break;
             }                    
         }
-        if (!x) return PunktEingabe();
+        if (!EingabeKorrekt) return PunktEingabe();
         System.out.println("Erstelle " + Anzahl + " Punkte...");
-        Punkte = new Punkt[Anzahl];
-        
-        for(int i = 0; i < Punkte.length; i++)
+        Punkte = new Punkt[Anzahl+1];
+        Punkte[0] = new Punkt();
+        for(int i = 1; i < Punkte.length; i++)
         {
-            System.out.println("Geben die Koordinaten des " + (i+1) + "ten Punktes ein!");
+            System.out.println("Geben die Koordinaten des " + i + "ten Punktes ein!");
             System.out.println("Format:x,y");
-            boolean Erkannt = false;
-            while (!Erkannt)
+            EingabeKorrekt = false;
+            while (!EingabeKorrekt)
             {
                 String Eingabe = Scan.next();
                 String[] Koordinaten = Eingabe.split(",",3);
                 
                 try
                 {
-                    int Zahl = Integer.valueOf(Koordinaten[0]);
-                    hier!!!!!//Punkte[i].setX(Zahl);
-                    Zahl = Integer.valueOf(Koordinaten[1]);
-                    //Punkte[i].setY(Zahl); 
-                    Erkannt = true;
+                    int x = Integer.valueOf(Koordinaten[0]);
+                    int y = Integer.valueOf(Koordinaten[1]);
+                    if (x >= 0 && x <= breite && y >= 0 && y <= hoehe)
+                    {
+                        Punkte[i] = new Punkt();
+                        Punkte[i].setX(x);
+                        Punkte[i].setY(y); 
+                        EingabeKorrekt = true;
+                    } else System.out.println("Punkt liegt nicht im Spielfeld(X:0..."+breite+" Y:=..."+hoehe);
                 }
                 catch (Exception E)
                 { 
-                    System.out.println("Fehlerhafte Eingabe!");
+                    System.out.println("Fehlerhafte Eingabe.");
                 }                    
             }
         }
         return Punkte;
+    }
+    
+    public void poiSortieren()
+    {
+        boolean Fertig = false;
+        while (!Fertig)
+        {
+            Fertig = true;
+            for (int i = 1; i < Punkte.length; i++)
+            { 
+                if (Punkte[i].gibAbstand(Punkte[0]) < Punkte[i-1].gibAbstand(Punkte[0]))
+                {
+                    Punkt P = new Punkt(Punkte[i].getX(), Punkte[i].getY());
+                    Punkte[i] = Punkte[i-1];
+                    Punkte[i-1] = P;
+                    Fertig = false;
+                }
+            }   
+        }
     }
 }
